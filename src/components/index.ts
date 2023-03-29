4c68591df6d5c89d190305dc3a53b451f041aa02
Gitarackur <peterosah5@gmail.com>
2023-03-29 18:28:32 +0100

    creating refine admin dashboard

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,7 @@
-export { Header } from "./header";
+export * from "./header";
+export * from "./title";
+export * from "./icons";
+export * from "./dashboard";
+export * from "./offLayoutArea";
+export * from "./map";
+
